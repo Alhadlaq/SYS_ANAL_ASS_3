6d54b142cf5aeeeb37427e7ea1b3935a3aaa329c
Alhadlaq <33044594+Alhadlaq@users.noreply.github.com>
2017-11-29 17:36:08 -0400

    v 4.4.1
    new test files

--- Simulation.cpp
+++ Simulation.cpp
@@ -68,6 +68,7 @@ void start_simulation(void)
 void print_reports()
 {
     FILE *output = fopen("output.txt", "w");
+    FILE *output2 = fopen("students_out.txt", "w");
     printf_graduated_students(output);
     fprintf(output, "-----------------------------------------------------------------\n");
     print_tuition(output);
@@ -76,7 +77,7 @@ void print_reports()
     fprintf(output, "-----------------------------------------------------------------\n");
     university_time_table(output);
     fprintf(output, "-----------------------------------------------------------------\n");
-    courses_taken_by_students(output);
+    courses_taken_by_students(output2);
 }
 
 void courses_taken_by_students (FILE *out)
